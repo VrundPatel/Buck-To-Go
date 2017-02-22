78304713451116a0aa92c9545a605135ae88f174
Robert Modrall <Robert Modrall>
2017-02-22 14:04:13 -0500

    Added a simple radio box w/options, and a submit button does nothing.

--- buckToGo/src/pages/payment-option/payment-option.ts
+++ buckToGo/src/pages/payment-option/payment-option.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-payment-option',
@@ -7,7 +7,20 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class PaymentOptionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController
+  ) {}
+
+  dismiss(){
+    this.viewCtrl.dismiss();
+  }
+
+  orderFood(){
+    alert("Hallo");
+    this.dismiss();
+  }
 
   // ionViewDidLoad() {
   //   console.log('ionViewDidLoad PaymentOptionPage');
