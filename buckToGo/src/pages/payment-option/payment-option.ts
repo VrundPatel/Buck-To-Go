73b43943218ab04df584df5798a75a7696d909f4
Robert Modrall <Robert Modrall>
2017-03-29 15:46:42 -0400

    Add the Credit card to radio with dropDown

--- buckToGo/src/pages/payment-option/payment-option.ts
+++ buckToGo/src/pages/payment-option/payment-option.ts
@@ -6,11 +6,18 @@ import { ViewController } from 'ionic-angular';
   templateUrl: 'payment-option.html'
 })
 export class PaymentOptionPage {
-
+paymentOptions: string [];
+paymentOptionChosen: string;
   constructor(
 
     public viewCtrl: ViewController
-  ) {}
+  ) {this.paymentOptions = [
+    'Cash', 'Points', 'Credit Card'
+  ]}
+
+  paymentOption(){
+    console.log('payment', this.paymentOptionChosen);
+  }
 
   dismiss(){
     this.viewCtrl.dismiss();
